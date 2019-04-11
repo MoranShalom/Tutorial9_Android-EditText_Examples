3943b5e12bf1f5ee68af7c2dd4d853d1ec019716
MoranShalom <moranshalom1234@gmail.com>
2019-04-11 19:39:13 +0300

    Android-EditText_Examples

---
+++ app/src/main/java/com/example/tutorial9_edittext_examples/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tutorial9_edittext_examples;
+//In this tutorial you will learn how to use different types of Android EditText and input types such as:
+// Android Multiline, Android Password, Android Email...etc
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
